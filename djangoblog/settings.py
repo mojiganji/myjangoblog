3e02f4a601f35b2d6fc19c342aebe07466e2f00b
mojiganji <mojganganji2009@gmail.com>
2022-07-02 22:10:32 +0430

    setting change

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -77,11 +77,8 @@ WSGI_APPLICATION = 'djangoblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'project22_db',
-        'USER': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
     }
 }
